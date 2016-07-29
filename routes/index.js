991043a99db97481e0a62fecfbe79d3c9a999308
soroush <sorooshvgr@gmail.com>
2016-07-28 18:04:45 -0700

    changed database name

--- routes/index.js
+++ routes/index.js
@@ -28,14 +28,14 @@ router.get('/bins', function(req, res) {
     			item.time = "Not logged yet";
     		else{
     			var t = new Date(item.time);
-    			item.time = dateFormat(t, "dddd, mmmm dS, yyyy, h:MM:ss TT Z");;
+    			item.time = dateFormat(t, "dddd, mmmm dS, yyyy, h:MM:ss TT");;
     		}
 
     		if(item.last_pickup == null)
     			item.last_pickup = "Never been picked up";
     		else{
     			var t = new Date(item.last_pickup);
-    			item.last_pickup = dateFormat(t, "dddd, mmmm dS, yyyy, h:MM:ss TT Z");;
+    			item.last_pickup = dateFormat(t, "dddd, mmmm dS, yyyy, h:MM:ss TT");;
     		}
     	})
     	
